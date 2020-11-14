4f29926bcf1e8c10cc116ba99222211ee913825c
nicolas <nicolas.kelepuris@gmail.com>
2020-11-14 17:19:43 -0300

    initial commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+    }
+}
